019be1c501bfd466a74af95db8fc8180d954bb35
hetviparekh01 <110492506+hetviparekh01@users.noreply.github.com>
2024-06-13 07:48:58 +0530

    added dynamic address feilds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { UserComponent } from './user/user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,10 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -13,11 +13,33 @@ export class UserComponent {
       userId:['',Validators.required],
       username:['',Validators.required],
       email:['',Validators.required],
-      addresses:this.formBuilder.array([])
+      addresses: this.formBuilder.array([this.createAddress()])
     })
   }
-
-
+  get addressList() :FormArray{
+    return this.userForm.get('addresses') as FormArray
+  }
+  createAddress() {
+    return this.formBuilder.group({
+      street: ['', Validators.compose([Validators.required])], 
+      city: [null, Validators.compose([Validators.required])], 
+      state: [null, Validators.compose([Validators.required])],
+      zipCode: [null, Validators.compose([Validators.required, Validators.pattern(/^\d{10}$/)])]
+    });
+  }
+  addAddress() {
+    this.addressList.push(this.createAddress());
+  }
+  removeAddress(i:number) {
+    this.addressList.removeAt(i);
+  }
+  submitForm(){
+    if(this.userForm.valid){
+      console.log(this.userForm.value);
+    }else{
+      alert("Error in filling the form!!")
+    }
+  }
   
 
 
